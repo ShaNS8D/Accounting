791e4651e28fd4332717b53229058ab6821df04a
Shalaevskiy Nikolay <nshalaevskiy@intraservices.ru>
2023-11-16 12:30:04 +0300

    corrected

--- main.py
+++ main.py
@@ -1,14 +1,11 @@
-import sys
 import datetime
-sys.path.insert(1, 'C:\\Users\\shalaevskiyns\\Documents\\Доки по учебе\\Homework\\Accounting\\application')
-import salary
-from db.people import get_employees
+from application.salary import calculate_salary
+from application.db.people import get_employees
 
 
 if __name__ == '__main__':
     time = datetime.date.today()
     print(time)
-    sal = salary.calculate_salary()
-    print(sal)
+    sal = calculate_salary()
     peo = get_employees()
-    print(peo)
+
